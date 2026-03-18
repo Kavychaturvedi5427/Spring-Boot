9bfdd1cc0591c723b9ba87b39c1ec451509a3fe9
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-03-19 00:31:07 +0530

    Create data.sql
    Dummy Data which will be inserted as soon as the spring application starts.

---
+++ RestApi/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO student (rollno, name, aadhar_number) VALUES (1, 'Aman Sharma', '123456789012');
+INSERT INTO student (rollno, name, aadhar_number) VALUES (2, 'Kavya Verma', '234567890123');
+INSERT INTO student (rollno, name, aadhar_number) VALUES (3, 'Rahul Singh', '345678901234');
+INSERT INTO student (rollno, name, aadhar_number) VALUES (4, 'Priya Mishra', '456789012345');
+INSERT INTO student (rollno, name, aadhar_number) VALUES (5, 'Arjun Gupta', '567890123456');
