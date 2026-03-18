6d97566b91c977468e91e95536d66688b3ebd04b
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-03-19 00:29:46 +0530

    Create StudentController.java
    Controller Class.

---
+++ RestApi/src/main/java/com/kavya/restapi/RestApi/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.kavya.restapi.RestApi.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kavya.restapi.RestApi.dto.StudentDTO;
+import com.kavya.restapi.RestApi.entity.Student;
+import com.kavya.restapi.RestApi.services.StudentServices;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentServices services;
+
+    @GetMapping("/")
+    @ResponseBody
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return ResponseEntity.status(HttpStatus.OK).body(services.getAllStudents());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<StudentDTO> addNewStudents(@RequestBody StudentDTO stuDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(services.addStudent(stuDto));
+    }
+
+}
