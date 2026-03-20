409e2ad75cd16143ce18e3521d40e2f6e9109807
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-03-20 23:48:48 +0530

    Create BloodGroupType.java

---
+++ HospitalManagement/Hospital-Management-System/src/main/java/com/kavya/hospitalmanagement/type/BloodGroupType.java
@@ -0,0 +1,14 @@
+package com.kavya.hospitalmanagement.type;
+
+public enum BloodGroupType {
+    A_POSITIVE,
+    B_POSITIVE,
+    C_POSITIVE,
+    D_POSITIVE,
+    E_POSITIVE,
+    F_POSITIVE,
+    G_POSITIVE,
+    H_POSITIVE,
+    O_POSITIVE;
+
+}
