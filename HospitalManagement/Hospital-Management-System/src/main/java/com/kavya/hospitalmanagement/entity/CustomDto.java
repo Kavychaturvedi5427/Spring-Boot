da3d12cfbf97d7e4e1ec126530cca6497f11f9d9
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-03-25 23:46:18 +0530

    Projection Class.

---
+++ HospitalManagement/Hospital-Management-System/src/main/java/com/kavya/hospitalmanagement/entity/CustomDto.java
@@ -0,0 +1,15 @@
+package com.kavya.hospitalmanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data       // generates all boilerplate code like getter/setters hashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustomDto {
+    private String name;
+    private String address;
+}
