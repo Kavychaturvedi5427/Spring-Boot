8d4eeba35aa2355b3906c8d299e856b8c0ef7de3
Kavya Chaturvedi <143157156+Kavychaturvedi5427@users.noreply.github.com>
2026-04-02 20:35:49 +0530

    Update PatientService.java

--- HospitalManagement/Hospital-Management-System/src/main/java/com/kavya/hospitalmanagement/service/PatientService.java
+++ HospitalManagement/Hospital-Management-System/src/main/java/com/kavya/hospitalmanagement/service/PatientService.java
@@ -1,5 +1,7 @@
 package com.kavya.hospitalmanagement.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ import lombok.RequiredArgsConstructor;
 public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
 
     @Transactional
     // if any error arises then all the changes will be rolled back to maintain the
